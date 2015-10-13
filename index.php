f7e588f88ac042aaa100381de0127b5e6fcab2fa
Jacob Goren <jacob@sfoc02nghkkg3qd.ads.autodesk.com>
2015-10-13 15:46:03 -0700

    Wrong home page
    Wrong home page

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
-<?php
-$data = array("callback" => "https://apoorva.com", "clientid" => "LfmvNs1g14p0C7AR3IKyfoCxO6i2zKAJ", "clientsecret" => "S6meLXlUY00YVML3");
-$data_string = json_encode($data);
-$ch = curl_init('https://m1g1mqu433.execute-api.us-east-1.amazonaws.com/prod/HMAC-Gen-For-Apigee-Lambda-Sample');                                                                      
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('x-api-key: pxovgQymYq7ezElYiHDpeaXM4x98DbqaMacpEIG2'));
-$result = curl_exec($ch);
-?>
+<html>
+ <head>
+  <title>ECS Docker Lambda Test</title>
+ </head>
+ <body>
+ <?php echo '<p>ECS Docker Lambda Test By Jacob Goren</p>'; ?> 
+ <form action="lambdasubmit.php" method="post">
+Callback: <input type="text" name="Callback"><br>
+ClientID: <input type="text" name="ClientID"><br>
+ClientSecret: <input type="text" name="ClientSecret"><br>
+<input type="submit">
+</form>
+ </body>
+</html>
